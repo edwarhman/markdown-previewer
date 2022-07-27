90af138cf0a7d02898b2e2b78dc01704c6c76362
Emerson Warhman <edwarhman@gmail.com>
2022-07-27 15:10:00 -0400

    refactor: remove marked object

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React from 'react';
-import { marked } from 'marked';
-import Markdown from 'marked-react';
+import React from 'react'
+import Markdown from 'marked-react'
 
 function App() {
-  console.log(marked);
   return (
     <div className="content">
       <Markdown>{'# Marked title'}</Markdown>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit';
-import previewerReducer from '../previewerSlice';
+import { configureStore } from '@reduxjs/toolkit'
+import previewerReducer from '../previewerSlice'
 
 export const store = configureStore({
   reducer: {
     previewer: previewerReducer,
   },
-});
+})
