f2fac26fe7bed28c280660d7515af2a8414a26f4
Emerson Warhman <edwarhman@gmail.com>
2022-07-27 14:17:42 -0400

    feat: add previewer state slice

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import {marked } from 'marked'
-import Markdown from 'marked-react'
+import { marked } from 'marked';
+import Markdown from 'marked-react';
+
 function App() {
-    console.log(marked)
+  console.log(marked);
   return (
     <div className="content">
-      <Markdown>
-        {"# Marked title"} 
-      </Markdown>
+      <Markdown>{'# Marked title'}</Markdown>
     </div>
   );
 }
