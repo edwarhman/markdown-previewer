17f7d28ea59958dadb5631f34b393e990c12ec31
Emerson Warhman <edwarhman@gmail.com>
2022-07-27 13:53:38 -0400

    feat: create editor and previewer components

---
+++ src/Previewer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Markdown from 'marked-react';
+
+export function Editor() {
+  return <textarea name="" id="" cols="30" rows="10"></textarea>;
+}
+
+export function Previewer() {
+  return (
+    <div>
+      <Markdown>{/* content there */}</Markdown>
+    </div>
+  );
+}
