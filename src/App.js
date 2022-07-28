cf48d176cbca41f5daf508567e045107334c5d6c
Emerson Warhman <edwarhman@gmail.com>
2022-07-28 12:15:16 -0400

    feat: add actions bars to sections

--- src/App.js
+++ src/App.js
@@ -3,9 +3,29 @@ import { Preview, Editor } from './Previewer'
 
 function App() {
   return (
-    <div>
-      <Editor></Editor>
-      <Preview></Preview>
+    <div className="appWrapper">
+      <div className="editorContainer container">
+        <header className="editorHeader header">
+          <h2>Editor</h2>
+          <nav className="actionsNav">
+            <a href="" onClick={(e) => e.preventDefault()}>
+              copy
+            </a>
+            <p>fullscreen</p>
+          </nav>
+        </header>
+        <Editor></Editor>
+      </div>
+      <div className="previewContainer container">
+        <header className="previewHeader header">
+          <h2>Preview</h2>
+          <nav className="actionsNav">
+            <a href="">print</a>
+            <p>fullscreen</p>
+          </nav>
+        </header>
+        <Preview></Preview>
+      </div>
     </div>
   )
 }
