e8cc9801c5b2176362d277a1e0edafcb21ab65a1
Emerson Warhman <edwarhman@gmail.com>
2022-07-28 22:03:02 -0400

    feat: add fullscreen buttons listeners

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Preview, Editor } from './Previewer'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { toogleFullscreen, fullscreenType } from './previewerSlice'
 
 function App() {
+    const fullscreen = useSelector(state => state.previewer.fullscreen)
     const dispatch = useDispatch()
 
   return (
     <div className="appWrapper" id='wrapper'>
-      <div className="editorContainer container">
+        <div className={`editorContainer container ${fullscreen === fullscreenType.PREVIEW? " disabled" : ""}`}>
         <header className="editorHeader header">
           <h2>Editor</h2>
           <nav className="actionsNav">
@@ -20,7 +21,7 @@ function App() {
         </header>
         <Editor></Editor>
       </div>
-      <div className="previewContainer container">
+        <div className={`previewContainer container ${fullscreen === fullscreenType.EDITOR? " disabled" : ""}`}>
         <header className="previewHeader header">
           <h2>Preview</h2>
           <nav className="actionsNav">
