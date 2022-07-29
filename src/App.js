0d22a98400575607d087e2a3d026b5439104fa86
Emerson Warhman <edwarhman@gmail.com>
2022-07-28 23:05:10 -0400

    style: format code

--- src/App.js
+++ src/App.js
@@ -4,29 +4,49 @@ import { useDispatch, useSelector } from 'react-redux'
 import { toogleFullscreen, fullscreenType } from './previewerSlice'
 
 function App() {
-    const fullscreen = useSelector(state => state.previewer.fullscreen)
-    const dispatch = useDispatch()
+  const fullscreen = useSelector((state) => state.previewer.fullscreen)
+  const dispatch = useDispatch()
 
   return (
-    <div className="appWrapper" id='wrapper'>
-        <div className={`editorContainer container ${fullscreen === fullscreenType.PREVIEW? " disabled" : ""}`}>
+    <div className="appWrapper" id="wrapper">
+      <div
+        className={`editorContainer container ${
+          fullscreen === fullscreenType.PREVIEW ? ' disabled' : ''
+        }`}
+      >
         <header className="editorHeader header">
           <h2>Editor</h2>
           <nav className="actionsNav">
             <a href="" onClick={(e) => e.preventDefault()}>
               copy
             </a>
-              <p id='editorFullscreen' className='fullscreenBtn' onClick={()=> dispatch(toogleFullscreen(fullscreenType.EDITOR))}>fullscreen</p>
+            <p
+              id="editorFullscreen"
+              className="fullscreenBtn"
+              onClick={() => dispatch(toogleFullscreen(fullscreenType.EDITOR))}
+            >
+              fullscreen
+            </p>
           </nav>
         </header>
         <Editor></Editor>
       </div>
-        <div className={`previewContainer container ${fullscreen === fullscreenType.EDITOR? " disabled" : ""}`}>
+      <div
+        className={`previewContainer container ${
+          fullscreen === fullscreenType.EDITOR ? ' disabled' : ''
+        }`}
+      >
         <header className="previewHeader header">
           <h2>Preview</h2>
           <nav className="actionsNav">
             <a href="">print</a>
-            <p id='previewFullscreen' className='fullscreenBtn' onClick={()=> dispatch(toogleFullscreen(fullscreenType.PREVIEW))}>fullscreen</p>
+            <p
+              id="previewFullscreen"
+              className="fullscreenBtn"
+              onClick={() => dispatch(toogleFullscreen(fullscreenType.PREVIEW))}
+            >
+              fullscreen
+            </p>
           </nav>
         </header>
         <Preview></Preview>
